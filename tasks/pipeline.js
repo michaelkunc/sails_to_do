22bf586fa24370a1466090c9e734d3b667d5c75d
michaelkunc <mjkunc@gmail.com>
2015-09-23 19:02:32 -0500

    view controller and model complete

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -15,23 +15,29 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  'styles/**/*.css'
+    'bower_components/bootswatch/dist/css/bootstrap.css',
+    'styles/**/*.css'
 ];
 
 
 // Client-side javascript files to inject in order
 // (uses Grunt-style wildcard/glob/splat expressions)
 var jsFilesToInject = [
-  
-  // Load sails.io before everything else
-  'js/dependencies/sails.io.js',
 
-  // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
+    // Load sails.io before everything else
+    'js/dependencies/sails.io.js',
 
-  // All of the rest of your client-side js files
-  // will be injected here in no particular order.
-  'js/**/*.js'
+    // Dependencies like jQuery, or Angular are brought in here
+    'js/dependencies/**/*.js',
+    '/bower_components/jquery/dist/jquery.js',
+    '/bower_components/angular/angular.js',
+    '/bower_components/angular-route/angular-route.js',
+
+    // All of the rest of your client-side js files
+    // will be injected here in no particular order.
+    '/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+    '/bower_components/bootstrap/dist/js/boostrap.js',
+    'js/**/*.js'
 ];
 
 
@@ -45,7 +51,7 @@ var jsFilesToInject = [
 // templates get spit out to the same file.  Be sure and check out `tasks/README.md`
 // for information on customizing and installing new tasks.
 var templateFilesToInject = [
-  'templates/**/*.html'
+    'templates/*.html'
 ];
 
 
@@ -54,11 +60,11 @@ var templateFilesToInject = [
 // (i.e. where the other Grunt tasks spit them out, or in some cases, where
 // they reside in the first place)
 module.exports.cssFilesToInject = cssFilesToInject.map(function(path) {
-  return '.tmp/public/' + path;
+    return '.tmp/public/' + path;
 });
 module.exports.jsFilesToInject = jsFilesToInject.map(function(path) {
-  return '.tmp/public/' + path;
+    return '.tmp/public/' + path;
 });
 module.exports.templateFilesToInject = templateFilesToInject.map(function(path) {
-  return 'assets/' + path;
+    return 'assets/' + path;
 });
